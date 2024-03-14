96d5d7b7f4c7df521a49348877f7061461383fa4
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-14 13:07:32 +1030

    prac3

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,5 +5,5 @@ int main () {
 int size1 = 4;
 double array1[size1] = {1,3,4,2};
 double minInArray = arrayMin(array1, size1);
-cout << minInArray << endl;
+cout << "The minimum value in the array is "<< minInArray << endl;
 }
