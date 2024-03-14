ce859418ed8945fe32e22cb04d3e61e7f0758e17
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-14 13:13:20 +1030

    prac3

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -2,7 +2,7 @@
 extern void modifyArray(double* array, int size, double value);
 int main() {
 int size2= 3;
-double array2[size2]={1,2,3};
-double value2 = 5;
+double array2[size2]={1.9,2.8,3.7};
+double value2 = 5.0;
 modifyArray(array2,size2,value2);
 }
