8c81843256ce1fe385c44b3d8c4a5ec64d9e964d
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-14 13:19:23 +1030

    prac3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+using namespace std;
 double* duplicateArray(double* array, int size) {
-    double newArray[size];
-    for (int i=0; i<size; i++) {
+    double* newArray = new double[size];
+    for (int i = 0; i < size; ++i) {
         newArray[i] = array[i];
     }
+    return newArray;
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 extern double* duplicateArray(double* array, int size);
 int main() {
-    int size3 = 3;
-    double array3[size3] = {3,4,5};
+    double originalArray[] = {1.2, 3.4, 5.6, 7.8, 9.0};
+    int size = sizeof(originalArray) / sizeof(originalArray[0]);
+    double* newArray = duplicateArray(originalArray, size);
+    cout << "New array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << newArray[i] << " ";
+    }
+    cout << endl;
+    delete[] newArray;
+
+    return 0;
 }
