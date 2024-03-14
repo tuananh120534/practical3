56c1d7fde2744635740d3319ef77da95af5dd3a5
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-14 13:06:39 +1030

    prac3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 double arrayMin(double* array, int size) {
-    int min = array[0];
+    double min = array[0];
     for (int i=0 ;i<size;i++) {
         if (array[i]<min) {
             min = array[i];
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,6 +4,6 @@ double arrayMin(double* array, int size);
 int main () {
 int size1 = 4;
 double array1[size1] = {1,3,4,2};
-int minInArray = arrayMin(array1, size1);
+double minInArray = arrayMin(array1, size1);
 cout << minInArray << endl;
 }
