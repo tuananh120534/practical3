59ee57c99380df554678312a598c01dce5fee22e
tuananh120534 <ndtanh.2005@gmail.com>
2024-03-14 13:23:45 +1030

    prac3

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 void modifyArray(double* array, int size, double value) {
-    std::cout << "Modified Array contents: ";
     for (int i = 0; i<size; i++) {
-        std::cout << array[i] + value << " ";
+        array[i] = array[i] + value;
     }
 }
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+using namespace std;
 extern void modifyArray(double* array, int size, double value);
 int main() {
-int size2= 3;
-double array2[size2]={1.9,2.8,3.7};
-double value2 = 5.0;
-modifyArray(array2,size2,value2);
+    const int size = 5;
+    double myArray[size] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    double value = 2.0;
+    modifyArray(myArray, size, value);
+    cout << "Modified array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << myArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
